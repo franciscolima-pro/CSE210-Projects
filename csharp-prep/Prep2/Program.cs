fd2dc3098e4543293f95e7aa5e09eccfc9a53bf5
Francisco Lima <franciscolimapro@gmail.com>
2024-04-25 20:27:45 -0300

    Project 3 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string percent = Console.ReadLine();
+        int percentage = int.Parse(percent);
+        int sob = percentage % 10;
+        string letter = " ";
+        string sign = " ";
+        
+        
+        if (percentage >= 90  ) {
+            letter = "A";
+        }
+        else if (percentage >= 80) {
+            letter = "B";
+        }
+        else if (percentage >= 70) {
+            letter = "C";
+        }
+        else if (percentage >= 60) {
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+
+        if (sob < 3 ) {
+            if (letter == "F") {
+                sign = "";
+            } else {
+                sign = "-";
+            }
+        }
+        else if (sob >= 7 ) {
+            if (letter == "A" && sob >= 7 || letter == "F" &&  sob >= 7) {
+                sign = "";
+            } else {
+                sign = "+";
+            }
+        }
+        else {
+            sign = " ";
+        }
+        
+        if (percentage >= 70 ) {
+            Console.WriteLine ($"Your grade is {letter}{sign}");
+            Console.WriteLine("Congratulations, You Passed!");
+        }
+        else {
+            Console.WriteLine ($"Your grade is {letter}{sign}");
+            Console.WriteLine("Sorry, you didn't pass that time!");
+            Console.WriteLine("But don't be discouraged, and try again next time!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Net;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    {   /*Getting the numbers*/
+        
+        /*Console.WriteLine("What is the magic number?");
+        string number = Console.ReadLine();
+        int response = int.Parse(number);*/
+        
+        int guess2 = -1;
+       
+        /*Building my Random number*/
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0, 999);
+
+        /*Console.WriteLine("What is your guess?");
+        
+        string guess = Console.ReadLine();
+        int guess2 = int.Parse(guess);*/
+
+        /*Buildind my if statment*/
+        
+        /*if (guess2 < response) {
+            Console.WriteLine("Higher!");
+        }
+        else if(guess2 > response) {
+            Console.WriteLine("Lower!");
+        }
+        else {
+            Console.WriteLine("You guessed it!");
+        }*/
+       
+        /*Building my loop*/
+        
+        while (guess2 != magicNumber) {
+            Console.WriteLine("What is your guess?");
+            string guess = Console.ReadLine();
+            guess2 = int.Parse(guess);
+            if (guess2 < magicNumber) {
+            Console.WriteLine("Higher!");
+            }
+            else if(guess2 > magicNumber) {
+            Console.WriteLine("Lower!");
+            }
+            else {
+            Console.WriteLine("You guessed it!");
+            }
+        }    
+    }   
 }
