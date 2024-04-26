ed05ee6fb9ebde16ef75adf460f3c176a86e489c
Francisco Lima <franciscolimapro@gmail.com>
2024-04-26 19:53:21 -0300

    Tasks completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,77 @@
 using System;
-
+using System.Transactions;
+using System.Collections.Generic; /*In addition for my collections*/
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> myNumbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished!"); /*My title*/
+        
+        /*Primary variables*/
+        
+        int enter = -1;
+        int sum = 0;
+
+        /*My loop for ask the numbers*/
+       
+        while (enter!=0) {
+        Console.WriteLine("Enter number: ");
+        string response = Console.ReadLine();
+        enter = int.Parse(response);
+        if (enter!=0) {
+            myNumbers.Add(enter);
+        }}
+        
+        /*sum the numbers with my variable sum*/
+        
+        foreach (int item in myNumbers) {
+            sum += item;
+        }
+
+        /*Select the first number of the list, if the next number is bigger than it, it will be replaced.*/
+
+        int largest = myNumbers[0];
+        
+        foreach (int item in myNumbers) {
+            if (item > largest) {
+                largest = item;
+            }
+        }
+        
+       /*Getting the smallest positive number*/ 
+        
+        int smallest = largest;
+
+        foreach (int item in myNumbers){
+            if ( item > 0 && item < smallest ) {
+                smallest = item;
+            }
+
+        } 
+        
+        /*Finding my average, wich is the sum of 'the numbers divided by the quantity iof numbers in my list'*/
+
+        double average = (double) sum / myNumbers.Count;
+
+        /*Printing the results*/
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"Smallest Number is: {smallest}");
+
+        /*Making a sorted list;*/ 
+
+        myNumbers.Sort();
+
+        Console.WriteLine("The sorted list is: ");
+
+        foreach (int item in myNumbers){
+            Console.WriteLine(item);         
+        } 
+
     }
+    
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void  welcomeFunction () {
+            Console.WriteLine("Welcome to program!");
+        }
+        
+
+        static string userName (){
+            Console.Write("Please enter your name: ");
+            string yourname = Console.ReadLine();
+            return yourname;
+        }
+        
+        static int userNumber (){
+            Console.Write("Please enter your favorite number: ");
+            string userinput = Console.ReadLine();
+            int yournumber = int.Parse(userinput);
+            return yournumber;
+        }
+
+        static int squareNumber () {
+            int squarepilot = userNumber();
+            int squareResult = squarepilot * squarepilot;
+            return squareResult;
+        }
+
+        static void displayresults () {
+            string name = userName();
+            int square = squareNumber();
+            Console.WriteLine($"{name}, the square of your number is: {square}");
+            
+        }
+        
+        welcomeFunction();
+        displayresults();
     }
 }
